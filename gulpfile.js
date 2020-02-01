5b75c5e7b0111d53bfe375fce971831f4671968f
Andrey Martemyanov <marty105k@gmail.com>
2020-02-01 23:01:00 +0300

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,72 @@
+var gulp = require('gulp');
+var pug = require('gulp-pug');
+var stylus = require('gulp-stylus');
+var browserSync = require('browser-sync').create();
+var minify = require('gulp-minify');
+var concat = require('gulp-concat');
+var plumber = require('gulp-plumber');
+var clean = require('gulp-clean');
+
+var reload = browserSync.reload;
+
+gulp.task('styl', () => {
+  return gulp.src('src/css/*.styl')
+  .pipe(plumber())
+  .pipe(stylus({
+    'include css': true,
+      compress: true
+  }))
+  .pipe(concat('main.css'))
+  .pipe(gulp.dest('dist'))
+  .pipe(browserSync.stream())
+})
+
+gulp.task('pug',() => {
+  return gulp.src('src/html/*.pug')
+  .pipe(plumber())
+  .pipe(pug({pretty: true}))
+  .pipe(gulp.dest('dist'))
+})
+
+gulp.task('js', () => {
+  return gulp.src('src/js/*.js')
+  .pipe(plumber())
+  .pipe(concat('main.js'))
+  .pipe(minify({
+    ext:{
+      source: '',
+      min:'.min.js'
+  }
+  }))
+  .pipe(gulp.dest('dist'))
+})
+
+gulp.task('clear-css', () => {
+  return gulp.src('dist/*.css', {read: false})
+  .pipe(clean());
+})
+
+gulp.task('clear-html', () => {
+  return gulp.src('dist/*.html', {read: false})
+  .pipe(clean());
+})
+
+gulp.task('clear-js', () => {
+  return gulp.src('dist/*.js', {read: false})
+  .pipe(clean());
+})
+
+gulp.task('watch', () => {
+  global.watch = true;
+  browserSync.init(['dist/*.css', 'dist/*.html'],{
+    server: "./dist/"
+  });
+  gulp.watch('src/css/*.styl', series('clear-css', 'styl'));
+  gulp.watch('src/html/*.pug', series('clear-html', 'pug'));
+  gulp.watch('src/js/*.js', series('clear-js', 'js'));
+  gulp.watch('dist/*.html').on("change", reload);
+})
+
+
+
+gulp.task('default', gulp.series('styl', 'pug', 'js', 'watch'));
